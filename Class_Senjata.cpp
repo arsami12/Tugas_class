03dbbb26fb8cc357d88b50a358d9e3ee1d74c176
kendi ardi <arsamisapper12@gmail.com>
2018-01-19 21:20:16 +0800

    Class Senjata

--- Class_Senjata.cpp
+++ Class_Senjata.cpp
@@ -2,6 +2,18 @@
 #include <string>
 using namespace std;
 
+class senjata{
+	public:
+		int jumlahSenjata;
+		string merkSenjata;
+		string beratSenjata;
+		string panjangSenjata;
+		string peluruSenjata;
+		string RataPeluruSenjata;
+		string kecepatanSenjata; 
+		void tembakan(string jarak_tembak);
+};
+
 int main(){
 	
 }
